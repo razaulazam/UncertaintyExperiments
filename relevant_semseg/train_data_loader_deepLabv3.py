d2ab2340d92802bc744015b4cb4c3757df16931a
razaulazam <razaulazam@outlook.com>
2020-09-16 21:43:40 +0200

    Relevant semantic segmentation files

---
+++ relevant_semseg/train_data_loader_deepLabv3.py
@@ -0,0 +1,14 @@
+from torchvision.datasets import Cityscapes
+from torch.utils.data import DataLoader
+from transform_deepLabv3 import *
+
+
+def train_loader(data_dir, batch_size):
+    """Takes in the transform and loads the training dataset from Cityscapes class in torch vision"""
+    combined_transform = RandomCropsTrain()
+    data_required = Cityscapes(data_dir, split='train', mode='fine', target_type='semantic', transform=None,
+                               target_transform=None, transforms=combined_transform)
+    city_data_loader = DataLoader(data_required, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
+
+    return city_data_loader
+
